5c38dde357bd5ff621199917fd1f9577a135350c
Jinho Park <smash4728@gmail.com>
2022-06-27 19:26:14 +0900

    홈뷰 리전 추가

--- Modules/ReseMusume.Modules.Home/HomeModule.cs
+++ Modules/ReseMusume.Modules.Home/HomeModule.cs
@@ -1,15 +1,23 @@
 ﻿using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Regions;
+using ReseMusume.Core;
 using ReseMusume.Modules.Home.Views;
 
 namespace ReseMusume.Modules.Home
 {
     public class HomeModule : IModule
     {
+        private readonly IRegionManager _regionManager;
+
+        public HomeModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
         public void OnInitialized(IContainerProvider containerProvider)
         {
-
+            _regionManager.RegisterViewWithRegion<Views.Home>(RegionNames.ContentRegion);
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
