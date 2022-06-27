80ab5907c951412201aff62213be8e6b1f271833
Jinho Park <smash4728@gmail.com>
2022-06-27 19:16:49 +0900

    첫 커밋
    프리즘 기반 코어 생성 홈 생성

---
+++ Modules/ReseMusume.Modules.Home/HomeModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using ReseMusume.Modules.Home.Views;
+
+namespace ReseMusume.Modules.Home
+{
+    public class HomeModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ ReseMusume/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using ReseMusume.Views;
+using System.Windows;
+using ReseMusume.Modules.Home;
+
+namespace ReseMusume
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<HomeModule>();
+
+            base.ConfigureModuleCatalog(moduleCatalog);
+        }
+    }
+}
