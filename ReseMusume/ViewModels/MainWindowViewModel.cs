8af0f977642458210e2ad3e446115cd5d96aba36
Jinho Park <smash4728@gmail.com>
2022-06-27 19:32:00 +0900

    타이틀명 변경

--- ReseMusume/ViewModels/MainWindowViewModel.cs
+++ ReseMusume/ViewModels/MainWindowViewModel.cs
@@ -4,11 +4,11 @@ namespace ReseMusume.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "ReseMusume";
         public string Title
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get => _title;
+            set => SetProperty(ref _title, value);
         }
 
         public MainWindowViewModel()
